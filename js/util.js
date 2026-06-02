08864f4af9d43ce049e98c09724ab11a3f515a11
PocoPluszak <czareksiedlecki404@gmail.com>
2026-06-02 19:56:53 +0200

    Replace Vue component with embed function for URLs
    Refactored the code to export an embed function for processing URLs instead of a Vue component.

--- js/util.js
+++ js/util.js
@@ -1,18 +1,21 @@
-export default {
-    props: {
-        video: {
-            type: String,
-            required: true,
-        },
-    },
-    template: `
-        <div class="embed">
-            <iframe :src="embedUrl" frameborder="0" allowfullscreen></iframe>
-        </div>
-    `,
-    computed: {
-        embedUrl() {
-            return `https://youtube.com{this.video}`;
-        },
-    },
-};
+export function embed(url) {
+    if (!url) return null;
+
+    // 1. Support Medal.tv clip links
+    if (url.includes('medal.tv')) {
+        if (url.includes('clip-embed')) return url;
+        let cleanUrl = url.split(/[?#]/)[0];
+        return cleanUrl.replace('medal.tv/games/', 'medal.tv/clip-embed/games/');
+    }
+
+    // 2. Original YouTube support logic
+    let id = url;
+    if (url.includes('youtu.be/')) {
+        id = url.split('youtu.be/')[1].split(/[?#]/)[0];
+    } else if (url.includes('://youtube.com')) {
+        id = url.split('://youtube.com')[1].split(/[?#]/)[0];
+    } else if (url.includes('v=')) {
+        id = url.split('v=')[1].split(/[&?#]/)[0];
+    }
+    return id;
+}
